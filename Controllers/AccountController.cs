5faf026332aa57b42c211e2ed7ba70343793ee91
Anujgiri Goswami <anujgoswami0212@gmail.com>
2026-09-15 14:37:43 +0530

    feat: implement user login

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -59,4 +59,48 @@ public class AccountController : Controller
 
         return View(model);
     }
+
+
+    [HttpGet]
+    public IActionResult Login(string? returnUrl = null)
+    {
+        ViewData["ReturnUrl"] = returnUrl;
+
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(
+        LoginViewModel model,
+        string? returnUrl = null)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        var result = await _signInManager.PasswordSignInAsync(
+            model.Email,
+            model.Password,
+            model.RememberMe,
+            lockoutOnFailure: false);
+
+        if (result.Succeeded)
+        {
+            if (!string.IsNullOrEmpty(returnUrl) &&
+                Url.IsLocalUrl(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        ModelState.AddModelError(
+            string.Empty,
+            "Invalid email or password.");
+
+        return View(model);
+    }
 }
