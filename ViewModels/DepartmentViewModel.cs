a444ee6935bce40937d5cf5442f05069c53a2f01
Anujgiri Goswami <anujgoswami0212@gmail.com>
2026-09-21 09:28:55 +0530

    feat: add DepartmentViewModel

---
+++ ViewModels/DepartmentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AssetHub.ViewModels;
+
+public class DepartmentViewModel
+{
+    public int DepartmentId { get; set; }
+
+    [Required]
+    [StringLength(100)]
+    [Display(Name = "Department Name")]
+    public string DepartmentName { get; set; } = string.Empty;
+
+    [Required]
+    [StringLength(20)]
+    [Display(Name = "Department Code")]
+    public string DepartmentCode { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    public string? Description { get; set; }
+
+    public int? ManagerEmployeeId { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
